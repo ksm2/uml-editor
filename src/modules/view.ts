e5756ddf20186298d135a55275f42180489c1f36
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2021-01-10 22:19:24 +0100

    Store view in Local Storage

--- src/modules/view.ts
+++ src/modules/view.ts
@@ -1,4 +1,5 @@
 import { StoreonStore } from "storeon";
+import { storageAvailable } from "../utils";
 
 export interface ViewState {
   grid: boolean;
@@ -8,8 +9,29 @@ export interface ViewEvents {
   "view/grid": boolean;
 }
 
-export function view(store: StoreonStore<ViewState, ViewEvents>) {
-  store.on("@init", () => ({ grid: false }));
+function loadView(): Partial<ViewState> {
+  if (storageAvailable()) {
+    const item = localStorage.getItem("view");
+    if (item !== null) return JSON.parse(item);
+  }
 
-  store.on("view/grid", (state, grid) => ({ ...state, grid }));
+  return {};
+}
+
+function storeView(state: ViewState): void {
+  if (storageAvailable()) {
+    localStorage.setItem("view", JSON.stringify(state));
+  }
+}
+
+export function view(store: StoreonStore<ViewState, ViewEvents>) {
+  store.on("@init", () => {
+    const { grid = false } = loadView();
+    return { grid };
+  });
+
+  store.on("view/grid", (state, grid) => {
+    storeView({ grid });
+    return { ...state, grid };
+  });
 }
